48ef90a18a58df4f503b4171da3c309e7c422f05
Martín Vásquez <pedromvasquezb@gmail.com>
2017-08-31 12:36:46 -0500

    Add tuto1

---
+++ tuto1/tuto1.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+#include <cstdlib>
+
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+using namespace std;
+
+void printDeviceInfo(bool withExtensions = false);
+void print(glm::mat4 matrix);
+bool initResources();
+void onDisplay();
+void onKeyPress(unsigned char key, int mouseX, int mouseY);
+
+int main(int argc, char* argv[]){
+    glutInit(&argc, argv);
+    glutInitContextVersion(2,0);
+    glutInitDisplayMode(GLUT_RGBA | GLUT_ALPHA | GLUT_DOUBLE | GLUT_DEPTH);
+    glutInitWindowSize(400, 400);
+    glutCreateWindow("Nuevo OpenGL");
+
+    GLenum glew_status = glewInit();
+    if(glew_status != GLEW_OK){
+        cout << "Error inicializando GLEW!" << endl;
+        exit(EXIT_FAILURE);
+    }
+
+    if(!GLEW_VERSION_2_0){
+        cout << "Su tajeta gráfica no soporta OpenGL 2.0" << endl;
+        exit(EXIT_FAILURE);
+    }
+
+    // printDeviceInfo();
+
+	glm::mat4 matrixA = glm::mat4(1.0);
+	// print(matrixA);
+	
+	float v[] = {0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3};
+	// glm::mat4 matrixB = glm::mat4();
+	glm::mat4 matrixB = glm::make_mat4(v);
+	// print(matrixB);
+	memcpy(glm::value_ptr(matrixB), v, sizeof(float) * 16);
+	// print(matrixB);
+	
+	if (initResources()) {
+		glutDisplayFunc(onDisplay);
+		glutKeyboardFunc(onKeyPress);
+		glutMainLoop();
+	}
+	
+    exit(EXIT_SUCCESS);
+}
+
+void printDeviceInfo(bool withExtensions) {
+	const GLubyte *renderer = glGetString(GL_RENDERER);
+    const GLubyte *vendor = glGetString(GL_VENDOR);
+    const GLubyte *version = glGetString(GL_VERSION);
+    const GLubyte *glslVersion = glGetString(GL_SHADING_LANGUAGE_VERSION);
+
+    GLint major, minor;
+    glGetIntegerv(GL_MAJOR_VERSION, &major);
+    glGetIntegerv(GL_MINOR_VERSION, &minor);
+
+    cout << "GL Vendor:" << vendor << endl;
+    cout << "GL Renderer:" << renderer << endl;
+    cout << "GL Version (string):" << version << endl;
+    cout << "GL Version (integer):" << major << ", " << minor << endl;
+    cout << "GLSL Version:" << glslVersion << endl;
+
+    if (withExtensions) {
+    	GLint nExtensions;
+	    glGetIntegerv(GL_NUM_EXTENSIONS, &nExtensions);
+
+	    for(int i = 0; i < nExtensions; i++){
+	        cout << glGetStringi(GL_EXTENSIONS, i) << endl;
+    	}
+    }
+}
+
+void print(glm::mat4 matrix) {
+	float *value_ptr = glm::value_ptr(matrix);
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			cout << *(value_ptr + i * 4 + j) << " ";
+		}
+		cout << endl;
+	}
+	cout << endl;
+}
+
+bool initResources() {
+	return true;
+}
+
+void onDisplay() {
+	glClearColor(0.5, 0.3, 1, 1);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glutSwapBuffers();
+}
+
+void onKeyPress(unsigned char key, int mouseX, int mouseY) {
+	switch (key) {
+		case 27:
+			glutLeaveMainLoop();
+			break;
+	}
+}
