a68318bb6d058895acddb22c792a92d419a11f05
Martín Vásquez <pedromvasquezb@gmail.com>
2017-12-03 22:17:16 -0500

    Rename 'sphere' variable to 'drawable' in 'trace' and 'render' functions Add 'make_unique' util function. Refactor shared_ptr into unique_ptr Add debug printing

--- ta.cpp
+++ ta.cpp
@@ -21,6 +21,7 @@
 #include <array>
 #include <iostream>
 #include <cassert>
+#include <memory>
 
 #if defined __linux__ || defined __APPLE__
 // "Compiled for Linux
@@ -31,6 +32,18 @@
 #endif
 
 #define DEFAULT_CENTER 123123
+#define DIFF_EPSILON 1e-8
+
+//[comment]
+// make_unique util. Taken from Herb Sutter's blog
+//[/comment]
+namespace std {
+  template<typename T, typename... Args>
+  std::unique_ptr<T> make_unique(Args&&... args)
+  {
+    return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+  }
+}
 
 template <typename T> class Vec3 {
 public:
@@ -188,10 +201,11 @@ public:
   std::array<Vec3f, 3> vertices;
   Vec3f normal;
 
-  Triangle() : Drawable() {}
   Triangle(Vec3f p0, Vec3f p1, Vec3f p2, const Vec3f &sc = 0, const float &refl = 0,
            const float &transp = 0, const Vec3f &ec = 0)
-        : vertices{p0, p1, p2}, Drawable((p0 + p1 + p2) / 3, sc, refl, transp, ec) {}
+        : vertices{p0, p1, p2}, Drawable((p0 + p1 + p2) / 3, sc, refl, transp, ec) {
+    normal = (p1 - p0).cross(p2 - p0);
+  }
 
   //[comment]
   // Compute ray-triangle intersection
@@ -199,6 +213,7 @@ public:
   bool intersect(const Vec3f &rayorig, const Vec3f &raydir, float& t,
                  float& t1) const override {
     float area2 = normal.length();
+    float denom = normal.dot(normal);
 
     //[comment]
     // Find point of intersection with plane
@@ -208,34 +223,48 @@ public:
     // Check if ray and plane are parallel
     //[/comment]
     float nDotRay = normal.dot(raydir);
-    if (nDotRay == 0.0) { // Use epsilon
+    if (abs(nDotRay) < DIFF_EPSILON) {
+      // std::cout << "parallel" << std::endl;
       return false;
     }
 
+    //[comment]
+    // Check if triangle is behind the ray
+    //[/comment]
     float d = normal.dot(vertices[0]);
     t = (normal.dot(rayorig) + d) / nDotRay;
     if (t < 0) {
+      // std::cout << "behind" << std::endl;
       return false;
     }
 
     Vec3f P = rayorig + t * raydir;
 
+    //[comment]
     // Inside-outside test
+    //[/comment]
     Vec3f C;
 
+    //[comment]
     // Edge 0
+    //[/comment]
     auto edge0 = vertices[1] - vertices[0];
     auto vp0 = P - vertices[0];
     C = edge0.cross(vp0);
     if (normal.dot(C) < 0) {
+      // std::cout << "Edge0" << std::endl;
       return false;
     }
 
+    //[comment]
     // Edge 1
+    //[/comment]
     auto edge1 = vertices[2] - vertices[1];
     auto vp1 = P - vertices[1];
     C = edge1.cross(vp1);
+    // u = C.length() / area2;
     if (normal.dot(C) < 0) {
+      // std::cout << "Edge1" << std::endl;
       return false;
     }
 
@@ -243,10 +272,17 @@ public:
     auto edge2 = vertices[0] - vertices[2];
     auto vp2 = P - vertices[2];
     C = edge2.cross(vp2);
+    // v = C.length() / area2;
     if (normal.dot(C) < 0) {
+      // std::cout << "Edge2" << std::endl;
       return false;
     }
 
+    // std::cout << "Intersects" << std::endl;
+    // for (const auto& v : vertices) {
+    //   std::cout << " " << v;
+    // }
+    // std::cout << std::endl;
     return true;
   }
 
@@ -272,33 +308,22 @@ public:
   std::vector<Face> faces;
 
   //Información para transformación inicial
-  // Vertex center;
   float scale;
-
-  //Matriz de transformación
-  // glm::mat4 model_transform;
-
-  // //Buffers para graficado
-  // GLfloat* object_vertices;
-  // GLfloat* object_normal;
-  // GLushort* object_indexes;
-
-  // //Id's para buffers
-  // GLuint vbo_object;
-  // GLuint vbo_normal;
-  // GLuint ibo_object;
 
   TriangleMesh(const Vec3f &sc = 0, const float &refl = 0,
                const float &transp = 0, const Vec3f &ec = 0)
     : Drawable(0, sc, refl, transp, ec) {}
 
   bool intersect(const Vec3f &rayorig, const Vec3f &raydir, float &t0,
-                 float &t1) const {
+                 float &t1) const override {
     unsigned triIndex;
     bool intersects = false;
     for (int i = 0; i < faces.size(); i++) {
       const auto& face = faces[i];
-      Triangle triangle(face.indices[0], face.indices[1], face.indices[2]);
+      // std::cout << vertices[face.indices[0]] << vertices[face.indices[1]] << vertices[face.indices[2]] << std::endl;
+      // std::cout << std::endl;
+      // exit(EXIT_SUCCESS);
+      Triangle triangle(vertices[face.indices[0]], vertices[face.indices[1]], vertices[face.indices[2]]);
       float tFront = INFINITY, tBack;
       if (triangle.intersect(rayorig, raydir, tFront, tBack) && tFront < t0) {
         t0 = tFront;
@@ -355,7 +380,7 @@ public:
     fclose(fid);
     mesh._center /= nverts;
 
-    // Calculate new center. Only if center
+    // Calculate new center
     if (center != DEFAULT_CENTER && center != mesh.center()) {
       mesh.move(center);
     }
@@ -436,32 +461,33 @@ float mix(const float &a, const float &b, const float &mix) {
 // the background color.
 //[/comment]
 Vec3f trace(const Vec3f &rayorig, const Vec3f &raydir,
-            const std::vector<Sphere> &spheres, const int &depth) {
+            const std::vector< std::unique_ptr<Drawable> > &drawables,
+            const int &depth) {
   // if (raydir.length() != 1) std::cerr << "Error " << raydir << std::endl;
   float tnear = INFINITY;
-  const Sphere *sphere = NULL;
-  // find intersection of this ray with the sphere in the scene
-  for (unsigned i = 0; i < spheres.size(); ++i) {
+  Drawable *drawable = nullptr;
+  // find intersection of this ray with the drawable in the scene
+  for (unsigned i = 0; i < drawables.size(); ++i) {
     float t0 = INFINITY, t1 = INFINITY;
-    if (spheres[i].intersect(rayorig, raydir, t0, t1)) {
+    if (drawables[i]->intersect(rayorig, raydir, t0, t1)) {
       if (t0 < 0)
         t0 = t1;
       if (t0 < tnear) {
         tnear = t0;
-        sphere = &spheres[i];
+        drawable = drawables[i].get();
       }
     }
   }
   // if there's no intersection return black or background color
-  if (!sphere)
+  if (!drawable)
     return Vec3f(2);
   // color of the ray/surfaceof the object intersected by the ray
   Vec3f surfaceColor = 0;
   Vec3f phit = rayorig + raydir * tnear; // point of intersection
-  Vec3f nhit = phit - sphere->center();    // normal at the intersection point
+  Vec3f nhit = phit - drawable->center();    // normal at the intersection point
   nhit.normalize();                      // normalize normal direction
   // If the normal and the view direction are not opposite to each other
-  // reverse the normal direction. That also means we are inside the sphere so
+  // reverse the normal direction. That also means we are inside the drawable so
   // set
   // the inside bool to true. Finally reverse the sign of IdotN which we want
   // positive.
@@ -469,7 +495,7 @@ Vec3f trace(const Vec3f &rayorig, const Vec3f &raydir,
   bool inside = false;
   if (raydir.dot(nhit) > 0)
     nhit = -nhit, inside = true;
-  if ((sphere->transparency > 0 || sphere->reflection > 0) &&
+  if ((drawable->transparency > 0 || drawable->reflection > 0) &&
       depth < MAX_RAY_DEPTH) {
     float facingratio = -raydir.dot(nhit);
     // change the mix value to tweak the effect
@@ -478,10 +504,10 @@ Vec3f trace(const Vec3f &rayorig, const Vec3f &raydir,
     // are already normalized)
     Vec3f refldir = raydir - nhit * 2 * raydir.dot(nhit);
     refldir.normalize();
-    Vec3f reflection = trace(phit + nhit * bias, refldir, spheres, depth + 1);
+    Vec3f reflection = trace(phit + nhit * bias, refldir, drawables, depth + 1);
     Vec3f refraction = 0;
-    // if the sphere is also transparent compute refraction ray (transmission)
-    if (sphere->transparency) {
+    // if the drawable is also transparent compute refraction ray (transmission)
+    if (drawable->transparency) {
       float ior = 1.1,
             eta = (inside) ? ior
                            : 1 / ior; // are we inside or outside the surface?
@@ -489,39 +515,39 @@ Vec3f trace(const Vec3f &rayorig, const Vec3f &raydir,
       float k = 1 - eta * eta * (1 - cosi * cosi);
       Vec3f refrdir = raydir * eta + nhit * (eta * cosi - sqrt(k));
       refrdir.normalize();
-      refraction = trace(phit - nhit * bias, refrdir, spheres, depth + 1);
+      refraction = trace(phit - nhit * bias, refrdir, drawables, depth + 1);
     }
-    // the result is a mix of reflection and refraction (if the sphere is
+    // the result is a mix of reflection and refraction (if the drawable is
     // transparent)
     surfaceColor = (reflection * fresneleffect +
-                    refraction * (1 - fresneleffect) * sphere->transparency) *
-                   sphere->surfaceColor;
+                    refraction * (1 - fresneleffect) * drawable->transparency) *
+                   drawable->surfaceColor;
   } else {
     // it's a diffuse object, no need to raytrace any further
-    for (unsigned i = 0; i < spheres.size(); ++i) {
-      if (spheres[i].emissionColor.x > 0) {
+    for (unsigned i = 0; i < drawables.size(); ++i) {
+      if (drawables[i]->emissionColor.x > 0) {
         // this is a light
         Vec3f transmission = 1;
-        Vec3f lightDirection = spheres[i].center() - phit;
+        Vec3f lightDirection = drawables[i]->center() - phit;
         lightDirection.normalize();
-        for (unsigned j = 0; j < spheres.size(); ++j) {
+        for (unsigned j = 0; j < drawables.size(); ++j) {
           if (i != j) {
             float t0, t1;
-            if (spheres[j].intersect(phit + nhit * bias, lightDirection, t0,
-                                     t1)) {
+            if (drawables[j]->intersect(phit + nhit * bias, lightDirection, t0,
+                                        t1)) {
               transmission = 0;
               break;
             }
           }
         }
-        surfaceColor += sphere->surfaceColor * transmission *
+        surfaceColor += drawable->surfaceColor * transmission *
                         std::max(float(0), nhit.dot(lightDirection)) *
-                        spheres[i].emissionColor;
+                        drawables[i]->emissionColor;
       }
     }
   }
 
-  return surfaceColor + sphere->emissionColor;
+  return surfaceColor + drawable->emissionColor;
 }
 
 //[comment]
@@ -530,7 +556,7 @@ Vec3f trace(const Vec3f &rayorig, const Vec3f &raydir,
 // the
 // sphere at the intersection point, else we return the background color.
 //[/comment]
-void render(const std::vector<Sphere> &spheres) {
+void render(const std::vector< std::unique_ptr<Drawable> > &spheres) {
   unsigned width = 640, height = 480;
   Vec3f *image = new Vec3f[width * height],
         *pixel = image;
@@ -566,23 +592,27 @@ void render(const std::vector<Sphere> &spheres) {
 // we render that scene, by calling the render() function.
 //[/comment]
 int main(int argc, char **argv) {
-  TriangleMesh mesh = TriangleMesh::fromOFF("NR0.off", Vec3f(0.0, -10004, -10));
+  TriangleMesh mesh = TriangleMesh::fromOFF("cube.off", Vec3f(0.0, 0, -10));
+  mesh.surfaceColor = Vec3f(0.32, 0.32, 0.36);
+  // mesh.reflection = 1;
+  mesh.transparency = 1;
   // srand48(13);
-  std::vector<Sphere> spheres;
+  std::vector< std::unique_ptr<Drawable> > spheres;
+  spheres.push_back(std::make_unique<TriangleMesh>(mesh));
   // position, radius, surface color, reflectivity, transparency, emission color
   spheres.push_back(
-      Sphere(Vec3f(0.0, -10004, -20), 10000, Vec3f(0.20, 0.20, 0.20), 0, 0.0));
+      std::make_unique<Sphere>(Sphere(Vec3f(0.0, -10004, -20), 10000, Vec3f(0.20, 0.20, 0.20), 0, 0.0)));
   spheres.push_back(
-      Sphere(Vec3f(0.0, 0, -20), 4, Vec3f(1.00, 0.32, 0.36), 1, 0.5));
+      std::make_unique<Sphere>(Sphere(Vec3f(0.0, 0, -20), 4, Vec3f(1.00, 0.32, 0.36), 1, 0.5)));
   spheres.push_back(
-      Sphere(Vec3f(5.0, -1, -15), 2, Vec3f(0.90, 0.76, 0.46), 1, 0.0));
+      std::make_unique<Sphere>(Sphere(Vec3f(5.0, -1, -15), 2, Vec3f(0.90, 0.76, 0.46), 1, 0.0)));
   spheres.push_back(
-      Sphere(Vec3f(5.0, 0, -25), 3, Vec3f(0.65, 0.77, 0.97), 1, 0.0));
+      std::make_unique<Sphere>(Sphere(Vec3f(5.0, 0, -25), 3, Vec3f(0.65, 0.77, 0.97), 1, 0.0)));
   spheres.push_back(
-      Sphere(Vec3f(-5.5, 0, -15), 3, Vec3f(0.90, 0.90, 0.90), 1, 0.0));
+      std::make_unique<Sphere>(Sphere(Vec3f(-5.5, 0, -15), 3, Vec3f(0.90, 0.90, 0.90), 1, 0.0)));
   // light
-  spheres.push_back(Sphere(Vec3f(0.0, 20, -30), 3, Vec3f(0.00, 0.00, 0.00), 0,
-                           0.0, Vec3f(3)));
+  spheres.push_back(
+      std::make_unique<Sphere>(Sphere(Vec3f(0.0, 20, -30), 3, Vec3f(0.00, 0.00, 0.00), 0, 0.0, Vec3f(3))));
   render(spheres);
 
   return 0;
